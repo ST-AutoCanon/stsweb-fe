a7384aa478fab6e0b70416f55847dd5b67b28cd5
NituRitu <vaibhavi>
2025-04-07 13:42:27 +0530

    mobile-features

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,22 @@
+// import type { CapacitorConfig } from '@capacitor/cli';
+
+// const config: CapacitorConfig = {
+//   appId: 'com.example.app',
+//   appName: 'sukalpatech-website',
+//   webDir: 'build'
+// };
+
+// export default config;
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'sukalpatech-website',
-  webDir: 'build'
+  appName: 'STS',
+  webDir: 'build',
+  server: {
+    hostname: 'localhost',
+    cleartext: true  // Allow non-https origin in development
+  }
 };
 
 export default config;
